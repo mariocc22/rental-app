e378504391767e50213babaec3fb1c46fbdd1a4f
Karan Singh Dhir <ksinghdhir1@gmail.com>
2023-02-14 00:52:31 -0800

    XYZ-120 add explore section with list

--- scripts/explore.js
+++ scripts/explore.js
@@ -1,1 +1,16 @@
-import '/styles/explore.css'
+import '/styles/explore.css';
+
+let params = new URLSearchParams(document.location.search);
+let activity = params.get("activity");
+
+// change view to find and list space
+if(activity) {
+    const mainMenu = document.getElementById("main-menu-activity")
+    mainMenu.classList.add('hide');
+
+    const listSection = document.getElementById("explore-section");
+    listSection.classList.remove('hide');
+
+    
+
+}
