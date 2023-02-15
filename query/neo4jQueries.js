dafcca410d7be0b0b6edcdbcb383ac9719d7ccc1
Karan Singh Dhir <ksinghdhir1@gmail.com>
2023-02-14 20:57:40 -0800

    XYZ-156 create filters properties on tags query

--- query/neo4jQueries.js
+++ query/neo4jQueries.js
@@ -1,5 +1,74 @@
 // File contains all neo4j queries used in Stage Application
 
-import {driver} from '../modules/neo4j.js'
+import { driver } from '../modules/neo4j.js'
 
-console.log(driver)
+function filterPlacesPrepQuery(params) {
+    let query = ''
+    if (params.coords) {
+        query = `
+        UNWIND $tags as tag
+        WITH tag
+        MATCH (p:Properties)<-[:TAGGED_WITH]-(t:Tag { name: tag })
+        WHERE point.distance(p.location, point({ longitude: $coords.longitude, latitude: $coords.latitude })) < $coords.distance
+        WITH p
+        RETURN COLLECT(p.id) as propertyIds`
+
+    } else {
+        query = `
+        UNWIND $tags as tag
+        WITH tag
+        MATCH (p:Properties)<-[:TAGGED_WITH]-(t:Tag { name: tag })
+        WITH p
+        RETURN COLLECT(p.id) as propertyIds`
+    }
+
+    return query;
+
+}
+
+/*
+params template:
+
+distance in metres
+tags are in key-value pair
+
+params example: 
+
+params = { tags: ['amenities-washroom'], coords: { latitude: 37.563534, longitude: -122.322269, distance: 2000 } }
+
+*/
+async function filterPlaces(params) {
+
+    const session = driver.session({ database: 'neo4j' });
+
+    try {
+
+        console.log(params)
+
+        // todo validation for params        
+
+        const readQuery = filterPlacesPrepQuery(params);
+        console.log(readQuery)
+
+        const readResult = await session.executeRead(tx =>
+            tx.run(readQuery, { ...params })
+        );
+
+        const propertyIds = readResult.records.map(record => {
+            const propertyIds = record.get('propertyIds');
+            return propertyIds
+        });
+
+        console.log(propertyIds)
+
+    } catch (error) {
+        console.log(error)
+        console.error('Somethign went wrong')
+    } finally {
+        await session.close()
+    }
+}
+
+export {
+    filterPlaces
+}
--- scripts/explore.js
+++ scripts/explore.js
@@ -1,15 +1,48 @@
 import '/styles/explore.css';
+import { filterPlaces } from '../query/neo4jQueries.js'
 
 let params = new URLSearchParams(document.location.search);
 let activity = params.get("activity");
 
+// required to filter list
+const selectedTags = [];
+const selectedCoords = {};
+
+
+
 // change view to find and list space
-if(activity) {
+if (activity) {
+
+    // push data to selectedTags
+    selectedTags.push(`activity-${activity}`)
+
+
     const mainMenu = document.getElementById("main-menu-activity")
     mainMenu.classList.add('hide');
 
     const listSection = document.getElementById("explore-section");
     listSection.classList.remove('hide');
+
+    // filter list based on musician activity
+    updatePlaces().then(list => {
+        // update it now
+        console.log(list)
+    })
+}
+
+
+// Filters places based on tags and displays updated 
+function updatePlaces() {
+
+    const payload = { tags: selectedTags }
+    // always append coords conditionally -> query doesn't have validaation
+    if (Object.values(selectedCoords).length) {
+        payload.coords = selectedCoords
+    }
+
+    return filterPlaces(payload);
+
+
 }
 
 const moreFilterElem = document.getElementById("more-filter");
@@ -17,3 +50,5 @@ const moreFilterElem = document.getElementById("more-filter");
 moreFilterElem.addEventListener("click", () => {
     alert('clicked')
 })
+
+console.log(filterPlaces)
