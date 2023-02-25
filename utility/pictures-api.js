68db731b1d4076768f6d0a599e0ab469a5d50532
Mario Cesena <112787277+mariocc22@users.noreply.github.com>
2023-02-25 09:09:32 -0800

    Adding the Select/Upload functionality
    Adding the Select/Upload functionality to the List my Space path

--- utility/pictures-api.js
+++ utility/pictures-api.js
@@ -1,16 +1,18 @@
 import "/styles/images-api.css";
 
 import {
-    app,
-    auth,
-    db,
-    doc,
-    setDoc,
-    getDoc,
-    getStorage,
-    sRef,
-    uploadBytesResumable,
-    getDownloadURL,
+  app,
+  auth,
+  db,
+  doc,
+  addDoc,
+  collection,
+  setDoc,
+  getDoc,
+  getStorage,
+  sRef,
+  uploadBytesResumable,
+  getDownloadURL,
 } from "../modules/firebase.js";
 
 // Initialize variables
@@ -28,93 +30,96 @@ let input = document.createElement("input");
 input.type = "file";
 
 input.onchange = (e) => {
-    files = e.target.files;
-    let extention = GetFileExt(files[0]);
-    let name = GetFileName(files[0]);
+  files = e.target.files;
+  let extention = GetFileExt(files[0]);
+  let name = GetFileName(files[0]);
 
-    namebox.value = name;
-    extlab.innerHTML = extention;
+  namebox.value = name;
+  extlab.innerHTML = extention;
 
-    reader.readAsDataURL(files[0]);
+  reader.readAsDataURL(files[0]);
 };
 
 reader.onload = function () {
-    myimg.src = reader.result;
+  myimg.src = reader.result;
 };
 
 SelBtn.onclick = function () {
-    input.click();
+  input.click();
 };
 
 function GetFileName(file) {
-    let temp = file.name.split(".");
-    let fname = temp.slice(0, -1).join(".");
-    return fname;
+  let temp = file.name.split(".");
+  let fname = temp.slice(0, -1).join(".");
+  return fname;
 }
 
 function GetFileExt(file) {
-    let temp = file.name.split(".");
-    let ext = temp.slice(temp.length - 1, temp.length);
-    return "." + ext[0];
+  let temp = file.name.split(".");
+  let ext = temp.slice(temp.length - 1, temp.length);
+  return "." + ext[0];
 }
 
 // Upload Process
 
 async function UploadProcess() {
-    let ImgToUpload = files[0];
-    let ImgName = namebox.value + extlab.innerHTML;
-    console.log(ImgName);
+  let ImgToUpload = files[0];
+  let ImgName = namebox.value + extlab.innerHTML;
+  console.log(ImgName);
 
-    const metaData = {
-        contentType: ImgToUpload.type,
-    };
-    const storage = getStorage();
-    const storageRef = sRef(storage, "Images/" + ImgName);
-    const uploadTask = uploadBytesResumable(storageRef, ImgToUpload, metaData);
+  const metaData = {
+    contentType: ImgToUpload.type,
+  };
+  const storage = getStorage();
+  const storageRef = sRef(storage, "Images/");
+  const uploadTask = uploadBytesResumable(storageRef, ImgToUpload, metaData);
 
-    uploadTask.on(
-        "state_changed",
-        (snapshot) => {
-            let progress =
-                (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-            proglab.innerHTML = "Upload " + progress + "%";
-        },
-        (error) => {
-            console.log(error.message);
-            alert("error: image not uploaded! ");
-        },
-        () => {
-            getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-                console.log(downloadURL);
-                SaveURLtoFirestore(downloadURL);
-            });
-        }
-    );
+  uploadTask.on(
+    "state_changed",
+    (snapshot) => {
+      // **********vThis is to see the upload progress
+      // let progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+      // proglab.innerHTML = "Upload " + progress + "%";
+    },
+    (error) => {
+      console.log(error.message);
+      alert("error: image not uploaded! ");
+    },
+    () => {
+      getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+        SaveURLtoFirestore(downloadURL);
+        console.log("Image uploaded successfully!");
+      });
+    }
+  );
 }
 
 // ... Post to DB //
 async function SaveURLtoFirestore(url) {
-    let name = namebox.value;
-    let ext = extlab.innerHTML;
-    let ref = doc(db, "ImageLinks/" + name);
+  // Here we can add the property ID, retrieving it from another module
+  let name = namebox.value;
+  // let ext = extlab.innerHTML;
+  // let ref = doc(db, "IMAGES");
 
-    await setDoc(ref, {
-        ImageName: name + ext,
-        ImageURL: url,
-    });
+  await addDoc(collection(db, "IMAGES"), {
+    propertyid: "property ID",
+    imageurl: url,
+  });
 }
 
 // ... Get from DB //
 async function GetImagefromFirestore() {
-    let name = namebox.value;
-    let ref = doc(db, "ImageLinks/" + name);
+  let name = namebox.value;
+  let ref = doc(db, "ImageLinks/" + name);
 
-    const docSnap = await getDoc(ref);
-    // myimg.src = docSnap.data().ImageURL;
-    if (docSnap.exists()) {
-        myimg.src = docSnap.data().ImageURL;
-    }
+  const docSnap = await getDoc(ref);
+  // myimg.src = docSnap.data().ImageURL;
+  if (docSnap.exists()) {
+    myimg.src = docSnap.data().imageurl;
+  }
 }
 
-UpBtn.onclick = UploadProcess;
-DownBtn.onclick = GetImagefromFirestore;
+// UpBtn.onclick = UploadProcess;
+// DownBtn.onclick = GetImagefromFirestore;
+
+export { input, UploadProcess, GetImagefromFirestore };
