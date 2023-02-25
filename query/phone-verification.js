64858a387b9161ed83ed8b2888de33b354c71eea
Mario Cesena <112787277+mariocc22@users.noreply.github.com>
2023-02-24 20:48:58 -0800

    Adding the phone verification page
    I haven't linked the page to any button, but it works, a SMS it's sent to your phone and once you validate the code it takes you to the home page by default

--- query/phone-verification.js
+++ query/phone-verification.js
@@ -1,21 +1,21 @@
-"use-strict";
+import "/styles/login.css";
 
 import {
-    app,
-    auth,
-    RecaptchaVerifier,
-    signInWithPhoneNumber,
+  app,
+  auth,
+  RecaptchaVerifier,
+  signInWithPhoneNumber,
 } from "../modules/firebase.js";
 
 // ==== Verification Phone ====
 // Create the recaptcha code container
 window.recaptchaVerifier = new RecaptchaVerifier(
-    "recaptcha-container",
-    {},
-    auth
+  "recaptcha-container",
+  {},
+  auth
 );
 recaptchaVerifier.render().then((widgetId) => {
-    window.recaptchaWidgetId = widgetId;
+  window.recaptchaWidgetId = widgetId;
 });
 
 const phoneForm = document.getElementById("verification-form");
@@ -29,37 +29,38 @@ const user_phone = document.getElementById("user-phone-succesful");
 // ============= Phone verification functionality ==============
 // Here we send the SMS to the phone number specified
 const sendVerificationCode = () => {
-    const appVerifier = window.recaptchaVerifier;
-    // If the message was successfully sent then print a message and proceed to log in with Phone with "btn_phone_login"
-    signInWithPhoneNumber(auth, phoneInput, appVerifier).then(
-        (confirmationResult) => {
-            const sentCodeId = confirmationResult;
-            console.log("Message sent!");
-            phoneForm.reset();
-            btn_phone_login.addEventListener("click", () =>
-                signInWithPhone(sentCodeId)
-            );
-        }
-    );
+  const appVerifier = window.recaptchaVerifier;
+  // If the message was successfully sent then print a message and proceed to log in with Phone with "btn_phone_login"
+  signInWithPhoneNumber(auth, phoneInput, appVerifier).then(
+    (confirmationResult) => {
+      const sentCodeId = confirmationResult;
+      console.log("Message sent!");
+      phoneForm.reset();
+      btn_phone_login.addEventListener("click", () =>
+        signInWithPhone(sentCodeId)
+      );
+    }
+  );
 };
 
 // Callback function from clicking the Log In button
 const signInWithPhone = (sentCodeId) => {
-    // The user type the code sent to his phone
-    const codeInput = phoneForm["verified-code"].value;
-    const code = codeInput;
-    sentCodeId
-        .confirm(code)
-        .then((result) => {
-            // if the code is correct then print something and display the user's phone
-            const user = result.user;
-            console.log(user);
-            phoneForm.reset();
-            user_phone.innerHTML = `${user.phoneNumber}`;
-        })
-        .catch((err) => {
-            console.log(err.message);
-        });
+  // The user type the code sent to his phone
+  const codeInput = phoneForm["verified-code"].value;
+  const code = codeInput;
+  sentCodeId
+    .confirm(code)
+    .then((result) => {
+      // if the code is correct then print something and display the user's phone
+      const user = result.user;
+      console.log(user);
+      phoneForm.reset();
+      user_phone.innerHTML = `${user.phoneNumber}`;
+      window.location.href = `index.html`;
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 };
 
 // Event listener of requesting the number to send the SMS
