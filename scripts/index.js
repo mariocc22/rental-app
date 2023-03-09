153f1e6f5d29405f16ec904d49d5fe4a5f233f6f
Mario Cesena <112787277+mariocc22@users.noreply.github.com>
2023-03-08 17:49:45 -0800

    Printing the Geolocation of the user
    Printing the Geolocation of the user in the Console (testing)

--- scripts/index.js
+++ scripts/index.js
@@ -2,13 +2,15 @@
 
 import "/styles/index.css";
 import { db, auth, onAuthStateChanged } from "../modules/firebase.js";
+import { whereAmI } from "../modules/geolocation.js";
 
 console.log("hello from index.js");
 
 // Buttons to access different paths
 const btn_explore = document.querySelector(".btn-explore"),
   btn_showcase = document.querySelector(".btn-showcase"),
-  btn_list_space = document.querySelector(".btn-list-space");
+  btn_list_space = document.querySelector(".btn-list-space"),
+  btn_geo = document.querySelector(".btn-geo");
 
 // User state (logged In or Logged Out)
 const userState = onAuthStateChanged(auth, (user) => {
@@ -28,3 +30,5 @@ const userState = onAuthStateChanged(auth, (user) => {
     console.log("No user logged!!");
   }
 });
+
+btn_geo.addEventListener("click", whereAmI);
