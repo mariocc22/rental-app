dbd931c0aad2b03cf2609dd45adb0d647df36806
Mario Cesena <112787277+mariocc22@users.noreply.github.com>
2023-03-13 22:20:07 -0700

    Multiple Files
    In this version, the functionality to add multiple files and set the featured image was implemented, the feature image selected, will be placed in the first index of the "_media" array. Additionally, the List my space flow, the page 1 and 2 were merged, still pending 1 more merged.

--- utility/pictures-api.js
+++ utility/pictures-api.js
@@ -26,29 +26,8 @@ const proglab = document.getElementById("upprogress");
 const SelBtn = document.getElementById("selbtn");
 const UpBtn = document.getElementById("upbtn");
 const DownBtn = document.getElementById("downbtn");
-let urlString = "";
-
-let input = document.createElement("input");
-input.type = "file";
-
-input.onchange = (e) => {
-  files = e.target.files;
-  let extention = GetFileExt(files[0]);
-  let name = GetFileName(files[0]);
-
-  namebox.value = name;
-  extlab.innerHTML = extention;
-
-  reader.readAsDataURL(files[0]);
-};
-
-reader.onload = function () {
-  myimg.src = reader.result;
-};
-
-SelBtn.onclick = function () {
-  input.click();
-};
+// let urlString = "";
+let urlString = [];
 
 function GetFileName(file) {
   let temp = file.name.split(".");
@@ -56,42 +35,133 @@ function GetFileName(file) {
   return fname;
 }
 
-function GetFileExt(file) {
-  let temp = file.name.split(".");
-  let ext = temp.slice(temp.length - 1, temp.length);
-  return "." + ext[0];
+// Original =======================
+// let input = document.createElement("input");
+// input.type = "file";
+
+// input.onchange = (e) => {
+//   files = e.target.files;
+//   let extention = GetFileExt(files[0]);
+//   let name = GetFileName(files[0]);
+//   namebox.value = name;
+//   extlab.innerHTML = extention;
+//   reader.readAsDataURL(files[0]);
+// };
+
+// reader.onload = function () {
+//   myimg.src = reader.result;
+// };
+// Original =======================
+
+// Create Input to handle multiple files =====================
+let input = document.createElement("input");
+input.type = "file";
+input.setAttribute("name", "myFile[]");
+input.setAttribute("multiple", "");
+
+input.addEventListener("change", handleFiles);
+
+function handleFiles() {
+  files = [...this.files];
+  files.forEach((file) => {
+    const reader = new FileReader();
+    reader.onload = function () {
+      let img = document.createElement("img");
+      img.src = reader.result;
+      console.log("Image created: ", img);
+      document
+        .querySelector(".image-container")
+        .insertAdjacentElement("afterbegin", img);
+    };
+
+    reader.readAsDataURL(file);
+  });
 }
+
+SelBtn.onclick = function () {
+  input.click();
+};
+
+// function GetFileExt(file) {
+//   let temp = file.name.split(".");
+//   let ext = temp.slice(temp.length - 1, temp.length);
+//   return "." + ext[0];
+// }
 
 // Upload Process
-async function UploadProcess() {
-  let ImgToUpload = files[0];
-  let ImgName = namebox.value + extlab.innerHTML;
-  console.log(ImgName);
+async function uploadAllFiles(src) {
+  console.log("before sorting: ", files);
 
-  const metaData = {
-    contentType: ImgToUpload.type,
-  };
-  const storage = getStorage();
-  const storageRef = sRef(storage, `IMAGES/${ImgName}`);
-  const uploadTask = uploadBytesResumable(storageRef, ImgToUpload, metaData);
-
-  uploadTask.on(
-    "state_changed",
-    (snapshot) => {},
-    (error) => {
-      console.log(error.message);
-      alert("error: image not uploaded! ");
-    },
-    () => {
-      getDownloadURL(uploadTask.snapshot.ref).then((_downloadURL) => {
-        // SaveURLtoFirestore(_downloadURL);
-        console.log("Image uploaded successfully!");
-        console.log(_downloadURL);
-        urlString = "" + _downloadURL;
-      });
-    }
-  );
+  Promise.all([files.map((file) => UploadProcess(file))])
+    .then((url) => {
+      console.log("uploading URL", url);
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 }
+
+async function UploadProcess(file) {
+  return new Promise(function (_res, _rej) {
+    let ImgToUpload = file;
+    let ImgName = GetFileName(ImgToUpload);
+    console.log("Image Name: ", ImgName);
+
+    const metaData = {
+      contentType: ImgToUpload.type,
+    };
+    const storage = getStorage();
+    const storageRef = sRef(storage, `IMAGES/${ImgName}`);
+    const uploadTask = uploadBytesResumable(storageRef, ImgToUpload, metaData);
+
+    uploadTask.on(
+      "state_changed",
+      (snapshot) => {},
+      (error) => {
+        alert("error: image not uploaded! ");
+      },
+      () => {
+        getDownloadURL(uploadTask.snapshot.ref).then((_downloadURL) => {
+          // SaveURLtoFirestore(_downloadURL);
+          console.log("Image uploaded successfully!");
+          console.log(_downloadURL);
+          // urlString = ("" + _downloadURL);
+          urlString.push("" + _downloadURL);
+        });
+      }
+    );
+  });
+}
+// ORIGINAL =======================
+// async function UploadProcess() {
+//   let ImgToUpload = files[0];
+//   let ImgName = namebox.value + extlab.innerHTML;
+//   console.log(ImgName);
+
+//   const metaData = {
+//     contentType: ImgToUpload.type,
+//   };
+//   const storage = getStorage();
+//   const storageRef = sRef(storage, `IMAGES/${ImgName}`);
+//   const uploadTask = uploadBytesResumable(storageRef, ImgToUpload, metaData);
+
+//   uploadTask.on(
+//     "state_changed",
+//     (snapshot) => {},
+//     (error) => {
+//       console.log(error.message);
+//       alert("error: image not uploaded! ");
+//     },
+//     () => {
+//       getDownloadURL(uploadTask.snapshot.ref).then((_downloadURL) => {
+//         // SaveURLtoFirestore(_downloadURL);
+//         console.log("Image uploaded successfully!");
+//         console.log(_downloadURL);
+//         urlString = "" + _downloadURL;
+//       });
+//     }
+//   );
+// }
 
 //////////////////////// PICTURE UPLOAD CAMERA
 async function cameraUpload(img, name) {
@@ -120,7 +190,7 @@ async function cameraUpload(img, name) {
         // SaveURLtoFirestore(_downloadURL);
         console.log("Image uploaded successfully!");
         console.log(_downloadURL);
-        urlString = "" + _downloadURL;
+        urlString.push("" + _downloadURL);
       });
     }
   );
@@ -155,4 +225,4 @@ async function cameraUpload(img, name) {
 // DownBtn.onclick = GetImagefromFirestore;
 
 // export { input, UploadProcess, GetImagefromFirestore, downloadURL};
-export { input, UploadProcess, urlString, cameraUpload };
+export { input, UploadProcess, urlString, cameraUpload, uploadAllFiles };
