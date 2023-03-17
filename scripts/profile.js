49ab2575f394a04950fa0f290bed3b1edc483cf0
Mario Cesena <112787277+mariocc22@users.noreply.github.com>
2023-03-17 01:56:51 -0700

    XYZ-243 Profile New Design Page

--- scripts/profile.js
+++ scripts/profile.js
@@ -1,23 +1,4 @@
 import "/styles/profile.css";
+import "/styles/standard-styles.css";
 
 ("use strict");
-
-// SPA Functionality
-const allPages = document.querySelectorAll("div.page");
-allPages[0].style.display = "block";
-
-function navigateToPage(event) {
-  const pageId = location.hash ? location.hash : "#page1";
-  for (let page of allPages) {
-    if (pageId === "#" + page.id) {
-      page.style.display = "block";
-    } else {
-      page.style.display = "none";
-    }
-  }
-  return;
-}
-navigateToPage();
-
-//init handler for hash navigation
-window.addEventListener("hashchange", navigateToPage);
