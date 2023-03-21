71bea73e489e0d40820951a1ff3981f27465e469
Karan Singh Dhir <ksinghdhir1@gmail.com>
2023-03-20 18:45:44 -0700

    XYZ-259 add loader and not found in explore spaces

--- scripts/explore-spaces.js
+++ scripts/explore-spaces.js
@@ -309,8 +309,12 @@ function prepareTags() {
 // Update List of Spaces
 
 async function displayProperties() {
-  // todo use the tags and properties
-  // todo use the string search
+
+  // display loader initially
+  const stageLoader = document.getElementById("stage-loader");
+  const noResultsFound = document.getElementById("no-results-found");
+  noResultsFound.classList.add("hide");
+  stageLoader.classList.remove("hide");
 
   // await to get the list from the database
 
@@ -328,11 +332,21 @@ async function displayProperties() {
     MY_COORDINATES.lat && MY_COORDINATES.lng ? MY_COORDINATES : undefined;
 
   const propertyIds = await filterPlaces(tags, price, string, distance, coords);
-
+  let foundProperty = false;
   // TODO add limit
 
   for (let propertyId of propertyIds) {
     const propertyInfo = await propertyFuncion(propertyId);
+
+    // hide loader
+    stageLoader.classList.add("hide")
+    // hide no results
+    noResultsFound.classList.add("hide");
+    // display list container
+    listContainer.classList.remove("hide")
+    foundProperty = true;
+
+
     if (propertyInfo) {
       const propertyObject = {
         imgs: propertyInfo?.media,
@@ -348,6 +362,14 @@ async function displayProperties() {
       populateList(listContainer, propertyObject);
     }
   }
+
+  if(!foundProperty) {
+    // hide loader
+    stageLoader.classList.add("hide")
+    // show no results 
+    noResultsFound.classList.remove("hide");
+  }
+
 }
 
 // function to populate list inside html script
